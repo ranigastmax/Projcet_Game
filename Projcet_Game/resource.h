aec4c0b04bed0cc5d6873e252ced6813c4ea94f5
K <rango.kuba@gmail.com>
2023-06-15 20:45:30 +0200

    test ostateczny

---
+++ Projcet_Game/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Plik do³¹czany wygenerowany przez œrodowisko Microsoft Visual C++.
+// U¿ywany przez: Projcet_Game.rc
+//
+#define IDI_ICON1                       105
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef _resource_rc
+#define _resource_rc
+
+MAINICON ICON "icon.ico" //If you can set any name
+
+#endif // _resource_rc
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
